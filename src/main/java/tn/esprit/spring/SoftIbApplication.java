e1f9798d8846a92aee287118361681e5da313bca
JenhaniChedli <chedli.jenhani@esprit.tn>
2021-07-17 13:55:28 +0200

    initial code de projet spring boot

---
+++ src/main/java/tn/esprit/spring/SoftIbApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftIbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftIbApplication.class, args);
+	}
+
+}
